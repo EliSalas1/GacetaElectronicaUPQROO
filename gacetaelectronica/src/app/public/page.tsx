eb71f00a537365596f7c77f3f34b38e91fd6086f
Tay tay <133799701+TayRyzenQ@users.noreply.github.com>
2025-06-10 18:13:38 -0500

    re-ordenamiento de carpeteas
    Se volvieron a ordenar las carpetas para que front trabaje de forma limpia

--- gacetaelectronica/src/app/public/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/public/page.tsx
-export default function PublicHome() {
-  return (
-    <main>
-      <h1>Bienvenido a la Gaceta Electrónica.</h1>
-      <p>Esta es una prueba para cambiar el texto de la primera pagina.</p>
-    </main>
-  );
-}
--- gacetaelectronica/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center text-sm text-gray-600 py-4 mt-10 border-t">
-      © {new Date().getFullYear()} Gaceta Electrónica UPQROO. Todos los derechos reservados.
-    </footer>
-  );
-}
