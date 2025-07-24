de2d2cdc22178b5304cb058bb67bf1032b05e4c7
robertoofb <robertojunio2004@gmail.com>
2025-07-24 11:09:53 -0500

    rol enlgobado en la sesión
    Ahora dependiendo del rol de la cuenta ya se puede acceder a la vista correspondiente:
    /private/administrador = Admin
    /private/redactor = Autor
    /private/supervisor = Revisor
    Se crearon 3 cuentas con las que te puedes loguear manualmente por si necesitan acceder, son las únicas cuentas que tienen la contraseña hasheada y la contraseña de las tres es '1234', revísenlas en la BD

--- gacetaelectronica/src/app/private/layout.tsx
+++ gacetaelectronica/src/app/private/layout.tsx
@@ -1,29 +1,13 @@
+// src/app/private/layout.tsx
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
 export default async function PrivateLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
-
   if (!session) {
-    redirect("/login"); // No autenticado
+    redirect("/publica/login"); // No autenticado
   }
-
-  const { role } = session.user;
-  const pathname = window.location.pathname; // Mover esta lógica al cliente (si es necesario)
-
-  // Restricciones según el rol
-  if (role === "Admin" && !pathname.startsWith("/private/administrador")) {
-    redirect("/forbidden");
-  }
-
-  if (role === "Revisor" && !pathname.startsWith("/private/supervisor")) {
-    redirect("/forbidden");
-  }
-
-  if (role === "Autor" && !pathname.startsWith("/private/redactor")) {
-    redirect("/forbidden");
-  }
-
+  // Ya no chequeamos rutas aquí: el middleware se encarga
   return <>{children}</>;
 }
